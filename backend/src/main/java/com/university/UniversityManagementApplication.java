6ccbf419709cd5237a3aa7ff76e07751f4aa5dce
younes <elalaouiyounes68@gmail.com>
2025-05-03 14:06:45 +0100

    Initial commit: Backend Spring Boot structure

---
+++ backend/src/main/java/com/university/UniversityManagementApplication.java
@@ -0,0 +1,13 @@
+package com.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityManagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/university/UniversityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
